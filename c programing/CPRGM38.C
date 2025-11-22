ce1abd654681ba18ea5dfcc3c8c71681559623ee
navyashreegovindswamy <navyashreegovindswamy@gmail.com>
2025-11-22 12:13:36 +0530

    Add files via upload

---
+++ c programing/CPRGM38.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    if(a > b)
+    
+        printf("%d is greater  \n", a);
+    else
+        printf("%d is greater  \n", b);
+    
+    return 0;
+}
